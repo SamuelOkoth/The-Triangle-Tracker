f14bc7cfabd3f3a74fcf22891e9136890c2a7862
SamuelOkoth <okothsamuel78@gmail.com>
2019-06-01 18:05:00 +0300

    indented my index.html

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,21 +2,16 @@ var a = parseFloat(document.getElementById('a').value);
 var b = parseFloat(document.getElementById('b').value);
 var c = parseFloat(document.getElementById('c').value);
 if ((a == NaN) || (b == NaN) || (c == NaN)) {
-    alert("Please insert valid dimension");
-}
-else if ((a === 0) || (b === 0) || (c === 0)) {
-    alert("Please insert valid dimension");
-}
-else if ((a + b <= c) || (b + c <= a) || (a + c <= b)) {
-    alert("Not a triangle .Try Again!");
-}
-else if ((a === b) || (b === c) || (a === c)) {
-    alert("Kudos ! isosceles triangle");
-}
-else if (a===b===c) {
-    alert("Kudos ! equilateral triangle");
-}
-else if ((a !== b) && (c !== a) && (b !== c)) {
-    alert("Kudos ! scalene triangle");
+  alert("Please insert valid dimension");
+} else if ((a === 0) || (b === 0) || (c === 0)) {
+  alert("Please insert valid dimension");
+} else if ((a + b <= c) || (b + c <= a) || (a + c <= b)) {
+  alert("Not a triangle .Try Again!");
+} else if ((a === b) || (b === c) || (a === c)) {
+  alert("Kudos ! isosceles triangle");
+} else if (a === b === c) {
+  alert("Kudos ! equilateral triangle");
+} else if ((a !== b) && (c !== a) && (b !== c)) {
+  alert("Kudos ! scalene triangle");
 }
 }
